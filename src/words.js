99699a563b2e21ee745fbeecb51e3e854df7573c
TaliV <tali.vulf@gmail.com>
2021-12-03 22:19:23 +0200

    add some word data

---
+++ src/words.js
@@ -0,0 +1,11 @@
+[
+    { akmuo: ['stone'] },
+    { namas: ['house', 'home'] },
+    { gėlė: ['flower'] },
+    { medus: ['honey'] },
+    { mokykla: ['school'] },
+    { kambarys: ['room'] },
+    { rašo: ['writes'] },
+    { kalba: ['speaks'] },
+    { myli: ['loves'] },
+]
